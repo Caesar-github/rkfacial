4b784e264cdd907229ab169842aa6bebbacdd1ae
Zhihua Wang <hogan.wang@rock-chips.com>
2020-07-20 16:11:47 +0800

    fix compile error on RK1808
    Signed-off-by: Zhihua Wang <hogan.wang@rock-chips.com>
    Change-Id: I08eb0b965106503648bb3d0fc0e40e8c0ebb54bc

--- rkdrm_display.c
+++ rkdrm_display.c
@@ -29,6 +29,7 @@
  * CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
  * SOFTWARE.
  */
+#include <libdrm/drm_mode.h>
 #include <stdbool.h>
 #include <stdint.h>
 #include <stddef.h>

0c21be947ec5e4d5a65fc3824f46508d04706d04
Zhihua Wang <hogan.wang@rock-chips.com>
2020-09-03 11:25:09 +0800

    modify check_isp_server_status
    Change-Id: I96ba1eaadcba7616d55e3dfc9ad619e597562909
    Signed-off-by: Zhihua Wang <hogan.wang@rock-chips.com>

--- rkfacial.c
+++ rkfacial.c
@@ -52,20 +52,33 @@
 #include "db_monitor.h"
 #include "rkfacial.h"
 
+#define RGB_CAM_ID 1
+#define IR_CAM_ID 0
+
 static int check_isp_server_status(int cam_id)
 {
     int fd;
+    int st = 0;
     char status[32];
     char pipe_name[128];
     sprintf(pipe_name, "/tmp/.ispserver_cam%d", cam_id);
 
-    fd = open(pipe_name, O_RDONLY);
-    if (fd < 0)
-        return 0;
-    memset(status, 0, sizeof(status));
-    read(fd, status, sizeof(status) - 1);
-    close(fd);
-    return atoi(status);
+    for (int i = 0; i < 10; i++) {
+        fd = open(pipe_name, O_RDONLY);
+        if (fd >= 0) {
+            memset(status, 0, sizeof(status));
+            read(fd, status, sizeof(status) - 1);
+            close(fd);
+            st = atoi(status);
+        }
+        if (st == 1)
+            break;
+        else
+            usleep(100000);
+    }
+    if (st != 1)
+        printf("%s: %s fail\n", __func__, pipe_name);
+    return st;
 }
 
 int rkfacial_init(void)
@@ -77,14 +90,8 @@ int rkfacial_init(void)
         printf("ispserver is not running!\n");
         return -1;
     }
-    while (check_isp_server_status(0) != 1) {
-        printf("%s: check isp server status\n", __func__);
-        usleep(10000);
-    }
-    while (check_isp_server_status(1) != 1) {
-        printf("%s: check isp server status\n", __func__);
-        usleep(10000);
-    }
+    check_isp_server_status(RGB_CAM_ID);
+    check_isp_server_status(IR_CAM_ID);
 #endif
 
     camrgb_control_init();
